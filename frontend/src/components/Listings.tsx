368f1816609a41734079507c4aad6147dc4f93d6
Abdurrahman Choudry <abdurrahman.choudry@mail.utoronto.ca>
2022-03-11 02:43:27 -0500

    GIV-68 fixed view listings page

--- frontend/src/components/Listings.tsx
+++ frontend/src/components/Listings.tsx
@@ -1,12 +1,12 @@
 import React, {useContext, useEffect, useState} from "react";
 import "../styles/listings.css";
-import {Alert, Button, Col, Form, Row} from "react-bootstrap";
+import {Alert, Button, Col, Form, Row, Table} from "react-bootstrap";
 import {TokenContext} from "../TokenContext";
 
 //Custom types for data iteration
 type Listing = {
-	listing_id?: number;
-	acc_id: number;
+    listing_id?: number;
+    acc_id: number;
     container: string;
     location: string;
     notes: string;
@@ -16,270 +16,336 @@ type Listing = {
 };
 
 type Item = {
-	name: string;
+    name: string;
     weight: number;
     expiry: Date;
-	tag_id: number;
-	tag_name: string;
+    tag_id: number;
+    tag_name: string;
 }
 
 
 //Props
 interface listingProps {
-	data: Array<Listing>,
-	deleteFunction: (number?: number) => any,
+    data: Array<Listing>,
+    deleteFunction: (number?: number) => any,
 
 }
 
 interface newListingProps {
-	data: Array<Item>,
+    data: Array<Item>,
 }
 
 interface templateProp {
-	add: boolean,
+    add: boolean,
 }
 
 type Form = {
-	container: string;
-	location: string,
-	notes: string;
+    container: string;
+    location: string,
+    notes: string;
 }
 
 //Listings Display Component: Generates the table of user listings.
 export default function Listing() {
 
-	const [tags, setTags] = useState<any[]>([]);
-	const [item, setItem] = useState<Item>({
-		expiry: new Date(),
-		name: "",
-		tag_id: 0,
-		tag_name: "",
-		weight: 0,
-	});
-	const [items, setItems] = useState<Item[]>([]);
-	const [form, setForm] = useState<Form>({
-		container: "",
-		notes: "",
-		location: ""
-	});
+    const [tags, setTags] = useState<any[]>([]);
+    const [item, setItem] = useState<Item>({
+        expiry: new Date(),
+        name: "",
+        tag_id: 0,
+        tag_name: "",
+        weight: 0,
+    });
+    const [items, setItems] = useState<Item[]>([]);
+    const [form, setForm] = useState<Form>({
+        container: "",
+        notes: "",
+        location: ""
+    });
+    const [formErrorMessage, setFormErrorMessage] = useState("");
+    const [itemErrorMessage, setItemErrorMessage] = useState("");
 
-	const [listings, setListings] = useState<Listing[]>([]);
+    const [listings, setListings] = useState<Listing[]>([]);
 
-	const { token } = useContext(TokenContext);
+    const {token} = useContext(TokenContext);
 
-	const loadTags = async () => {
-		const allItemsResponse = await fetch("/tag/all/", {
-			method: "GET",
-			headers: {
-				authorization: `Bearer ${token}`,
-			},
-		});
+    const loadTags = async () => {
+        const allItemsResponse = await fetch("/tag/all/", {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`,
+            },
+        });
 
-		if (allItemsResponse.status !== 200) {
-			return {};
-		} else {
-			let allTags = await allItemsResponse.json();
-			setItem((old) => {
-				old.tag_id = allTags[0].tag_id;
-				old.tag_name = allTags[0].name;
-				return old;
-			})
-			setTags(allTags);
-		}
-	}
+        if (allItemsResponse.status !== 200) {
+            return {};
+        } else {
+            let allTags = await allItemsResponse.json();
+            setItem((old) => {
+                old.tag_id = allTags[0].tag_id;
+                old.tag_name = allTags[0].name;
+                return old;
+            })
+            setTags(allTags);
+        }
+    }
 
-	const loadListings = async () => {
-		const allListings = await fetch("/listing/", {
-			method: "GET",
-			headers: {
-				authorization: `Bearer ${token}`,
-			},
-		});
+    const loadListings = async () => {
+        const allListings = await fetch("/listing/", {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${token}`,
+            },
+        });
 
-		setListings(await allListings.json());
-	}
-	
-	const createListing = async () => {
-		  
-		let newListing = {...form, createdAt: new Date(), updatedAt: new Date()};
-		let newItems = Array<any>();
-		  
-		for (let i of items) {
-			let newItem = {...i,  createdAt: new Date(), updatedAt: new Date()}
-			newItems.push(newItem);
-		}
+        setListings(await allListings.json());
+    }
 
-		const packagedListing = {
-			listing: newListing,
-			items: newItems,
-		}
-		
-		console.log(packagedListing)
-		let result = await fetch("/listing/", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				authorization: `Bearer ${token}`,
-			},
-			body: JSON.stringify(packagedListing)
-		});
-	}
+    const validateFormInput = () => {
+        if (form.container === "") {
+            return setFormErrorMessage("Please provide a container!");
+        }
+        if (form.location === "") {
+            return setFormErrorMessage("Please provide a location!")
+        }
+        if (items.length === 0) {
+            return setFormErrorMessage("Please add items to your listing!")
+        }
 
-	useEffect(() => {
-		loadTags().then(value => console.log("loaded tags"));
-		loadListings().then(value => console.log("loaded your listings"));
-	}, []);
+        setFormErrorMessage("");
+    }
 
-	return (
-		<div className="container mt-5">
-			<h1>Create a listing</h1>
-		<Form>
-			<Row className="mb-3">
-				<Form.Group as={Col}>
-					<Form.Label>Container</Form.Label>
-					<Form.Control type="text" placeholder="container" onChange={(e) => {
-						setForm((old) => {
-							old.container = e.target.value
-							return old;
-						});
-					}}/>
-				</Form.Group>
+    const validateItemInput = () => {
+        if (item.name == "") {
+            return setItemErrorMessage("Please enter an item name!");
+        }
+        if (items.filter(e => e.name === item.name).length > 0) {
+            return setItemErrorMessage("That item is already in this listing!");
+        }
+        if (item.weight <= 0) {
+            return setItemErrorMessage("Weight should be a positive number!");
+        }
+        if (item.expiry < new Date()) {
+            return setItemErrorMessage("The expiry date can't be in the past!");
+        }
+        setItemErrorMessage("");
 
-				<Form.Group as={Col} controlId="formGridPassword">
-					<Form.Label>Location</Form.Label>
-					<Form.Control type="text" placeholder="location" onChange={(e) => {
-						setForm((old) => {
-							old.location = e.target.value
-							return old;
-						});
-					}}/>
-				</Form.Group>
-			</Row>
+        setItems([...items, item]);
+        setItem({
+            weight: 0,
+            name: "",
+            expiry: new Date(),
+            tag_id: 0,
+            tag_name: ""
+        })
 
-			<Row>
-			<Form.Group className="mb-3" controlId="formGridAddress1">
-				<Form.Label>Notes</Form.Label>
-				<Form.Control as="textarea"
-							  type="text"
-							  placeholder="add any details you think are necessary"
-							  onChange={(e) => {
-								  setForm((old) => {
-									  old.notes = e.target.value
-									  return old;
-								  });
-							  }}/>
-			</Form.Group>
-			</Row>
+    }
 
-			<div className="border p-5 rounded">
-				<h4>Add an item to the listing</h4>
-			<Row>
-				<Form.Group as={Col} controlId="formGridPassword">
-					<Form.Label>Name</Form.Label>
-					<Form.Control type="text" placeholder="name" onChange={(e) => {
-						setItem((old) => {
-							old.name = e.target.value;
-							return old;
-						})
-					}}/>
-				</Form.Group>
-				<Form.Group as={Col} controlId="formGridPassword">
-					<Form.Label>Weight (kg)</Form.Label>
-					<Form.Control type="number" placeholder="weight" onChange={(e) => {
-						setItem((old) => {
-							old.weight = +e.target.value;
-							return old;
-						})
-					}}/>
-				</Form.Group>
-				<Form.Group as={Col}>
-					<Form.Label>Expiry</Form.Label>
-					<Form.Control type="date" placeholder="expiry date" onChange={(e) => {
-						setItem((old) => {
-							old.expiry = new Date(e.target.value);
-							return old;
-						})
-					}}/>
-				</Form.Group>
-				<Form.Group as={Col} controlId="formGridPassword">
-					<Form.Label>Tag</Form.Label>
-					<Form.Select
-						onChange={(e) => {
-							setItem((old) => {
-								old.tag_id = +e.target.value;
-								old.tag_name = e.target.id;
-								return old;
-							});
-						}}>
-						{tags.map(tag => (
-							<option
-								value={tag.tag_id}
-								id={tag.name}>
-								{tag.name}
-							</option>
-						))}
-					</Form.Select>
-				</Form.Group>
-			</Row>
-			<Button className="mt-2" onClick={(e) => {
-				console.log(item)
-				setItems([...items, item]);
-				setItem({weight: 0, name: "", expiry: new Date(), tag_id: 0, tag_name: ""})
-			}}>
-				Add item
-			</Button>
-		</div>
-		</Form>
-			<table className="table table-bordered">
-				<thead>
-				<tr>
-					<th scope="col">Name</th>
-					<th scope="col">Weight</th>
-					<th scope="col">Expiry</th>
-					<th scope="col">Tag</th>
-				</tr>
-				</thead>
-				<tbody>
-				{items.map(object => (
-					<tr>
-					<td> {object.name} </td>
-						<td> {object.weight} </td>
-						<td> {object.expiry.toDateString()} </td>
-						<td> {object.tag_name} </td>
-					</tr>
+    const createListing = async () => {
 
-				))}
-				</tbody>
-			</table>
-			<Button className="mt-2" onClick={createListing}>
-				Create new listing
-			</Button>
-			<br /><br /><br /><br /><br />
-			<h4>Your listings</h4>
-			<table className="table table-bordered">
-				<thead>
-				<tr>
-					<th scope="col">Listing ID</th>
-					<th scope="col">Container</th>
-					<th scope="col">Location</th>
-					<th scope="col">Notes</th>
-					<th scope="col">Items</th>
-				</tr>
-				</thead>
-				<tbody>
-				{listings.map(object => (
-					<tr>
-					<td> {object.listing_id} </td>
-						<td> {object.container} </td>
-						<td> {object.location} </td>
-						<td> {object.notes} </td>
-						<td> hai </td>
-					</tr>
+        let newListing = {
+            ...form,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+        let newItems = Array<any>();
 
-				))}
-				</tbody>
-			</table>
-		</div>
-	)
+        for (let i of items) {
+            let newItem = {...i, createdAt: new Date(), updatedAt: new Date()}
+            newItems.push(newItem);
+        }
+
+        const packagedListing = {
+            listing: newListing,
+            items: newItems,
+        }
+
+        console.log(packagedListing)
+        let result = await fetch("/listing/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(packagedListing)
+        });
+    }
+
+    useEffect(() => {
+        loadTags().then(value => console.log("loaded tags"));
+        loadListings().then(value => console.log("loaded your listings"));
+    }, []);
+
+    return (
+        <div className="container mt-5">
+            <h1>Create a listing</h1>
+            {formErrorMessage ? <Alert variant="danger">
+                {formErrorMessage}
+            </Alert> : null}
+            <Form>
+                <Row className="mb-3">
+                    <Form.Group as={Col}>
+                        <Form.Label>Container</Form.Label>
+                        <Form.Control type="text" placeholder="container"
+                                      value={form.container}
+                                      onChange={(e) => {
+                                          setForm({
+                                              ...form,
+                                              container: e.target.value
+                                          });
+                                      }}/>
+                    </Form.Group>
+
+                    <Form.Group as={Col}>
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control type="text" placeholder="location"
+                                      value={form.location}
+                                      onChange={(e) => {
+                                          setForm({
+                                              ...form,
+                                              location: e.target.value
+                                          });
+                                      }}/>
+                    </Form.Group>
+                </Row>
+
+                <Row>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Notes</Form.Label>
+                        <Form.Control as="textarea"
+                                      type="text"
+                                      placeholder="add any details you think are necessary"
+                                      value={form.notes}
+                                      onChange={(e) => {
+                                          setForm({
+                                              ...form,
+                                              notes: e.target.value
+                                          });
+                                      }}/>
+                    </Form.Group>
+                </Row>
+
+                <div className="border p-5 rounded">
+                    <h4>Add an item to the listing</h4>
+                    {itemErrorMessage ? <Alert variant="danger">
+                        {itemErrorMessage}
+                    </Alert> : null}
+                    <Row>
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="name"
+                                          value={item.name}
+                                          onChange={(e) => {
+                                              setItem({
+                                                  ...item,
+                                                  name: e.target.value
+                                              })
+                                          }}/>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Weight (kg)</Form.Label>
+                            <Form.Control type="number" placeholder="weight"
+                                          value={item.weight}
+                                          onChange={(e) => {
+                                              setItem({
+                                                  ...item,
+                                                  weight: +e.target.value
+                                              })
+                                          }}/>
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Label>Expiry</Form.Label>
+                            <Form.Control type="date" placeholder="expiry date"
+                                          onChange={(e) => {
+                                              setItem({
+                                                  ...item,
+                                                  expiry: new Date(e.target.value)
+                                              })
+                                          }}/>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Tag</Form.Label>
+                            <Form.Select
+                                onChange={(e) => {
+                                    setItem((old) => {
+                                        old.tag_id = +e.target.value;
+                                        old.tag_name = e.target.id;
+                                        return old;
+                                    });
+                                }}>
+                                {tags.map((tag, i) => (
+                                    <option
+                                        selected={i === 0 && !tag.tag_id}
+                                        value={tag.tag_id}
+                                        id={tag.name}>
+                                        {tag.name}
+                                    </option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+                    <Button className="mt-2" onClick={(e) => {
+                        validateItemInput();
+                    }}>
+                        Add item
+                    </Button>
+                </div>
+                <div className="border p-5 rounded mt-3">
+                    <h4>Items in the current listing</h4>
+                    <Table striped bordered hover responsive>
+                        <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Weight</th>
+                            <th scope="col">Expiry</th>
+                            <th scope="col">Tag</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {items.map(object => (
+                            <tr>
+                                <td> {object.name} </td>
+                                <td> {object.weight} </td>
+                                <td> {object.expiry.toDateString()} </td>
+                                <td> {object.tag_name} </td>
+                            </tr>
+
+                        ))}
+                        </tbody>
+                    </Table>
+                </div>
+                <Button className="mt-2" onClick={validateFormInput}>
+                    Create new listing
+                </Button>
+            </Form>
+
+            <div className="container mt-5">
+                <h4>Your listings</h4>
+                {listings.length > 0 ? <Table striped bordered hover responsive>
+                    <thead>
+                    <tr>
+                        <th scope="col">Listing ID</th>
+                        <th scope="col">Container</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Notes</th>
+                        <th scope="col">Items</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {listings.map(object => (
+                        <tr>
+                            <td> {object.listing_id} </td>
+                            <td> {object.container} </td>
+                            <td> {object.location} </td>
+                            <td> {object.notes} </td>
+                            <td> hai</td>
+                        </tr>
+
+                    ))}
+                    </tbody>
+                </Table> : "You have no listings. Fill out the form above to add one!"}
+            </div>
+        </div>
+    )
 
 }
--- frontend/src/components/ViewListings.tsx
+++ frontend/src/components/ViewListings.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { ListGroup, Container, Dropdown } from "react-bootstrap";
+import React, {useContext, useEffect, useState} from 'react'
+import {
+    ListGroup,
+    Container,
+    Dropdown,
+    Accordion,
+    Table
+} from "react-bootstrap";
 import "../styles/viewlistings.css";
+import {TokenContext} from "../TokenContext";
 
 type Listing = {
-	listing_id?: number;
-	acc_id: number;
+    listing_id: number;
+    acc_id: number;
     container: string;
     location: string;
     notes: string;
@@ -19,30 +26,43 @@ type Listing = {
 const ViewListings = () => {
 
     // This state will be used for storing data retrieved from request for all listings
-    const [allListings, setAllListings] = useState<Array<Listing>>([] as Listing[]);
-
+    const [allListings, setAllListings] = useState<Listing[]>([] as Listing[]);
+    const {token} = useContext(TokenContext);
 
     // Makes requests on component load and stores in state ^
     useEffect(() => {
 
         async function getListings() {
-            const response = await fetch("/listing/all" , {
+            const response = await fetch("/listing/all", {
                 method: "GET",
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
             });
 
-            let objs: Array<Listing> = await response.json();
+            let all_listings = await response.json();
             // Need to query user data: wait for backend overhaul next sprint
 
-            /*
-            objs.forEach((obj: any) => {
-                obj.acc_name = "John's Bakery"
-                obj.address = "123 Street"
-                obj.expiry.forEach((date: Date, index: number) => {
-                    obj.expiry[index] = new Date(date);
-                })
-            })
-            */
-            setAllListings(objs)
+            const listings = [];
+            for (let listing of all_listings) {
+                let new_listing: Listing = {} as Listing;
+                let {
+                    listing_id, container, acc_id, location, notes, createdAt,
+                    updatedAt
+                } = listing.dataValues;
+                new_listing.listing_id = listing_id;
+                new_listing.container = container;
+                new_listing.acc_id = acc_id;
+                new_listing.location = location;
+                new_listing.notes = notes;
+                new_listing.updatedAt = updatedAt;
+                new_listing.createdAt = createdAt;
+                new_listing.items = listing.item;
+                new_listing.user = listing.owner;
+                listings.push(new_listing);
+            }
+            setAllListings(listings);
+            console.log(listings);
         }
 
         getListings();
@@ -50,45 +70,52 @@ const ViewListings = () => {
     }, [])
 
     // If listings retrieved then render listings view
-    if (allListings) {
-        return (
-            <Container id="lv">
-                {allListings.map(listing => 
-                    <ListGroup key={listing.listing_id} horizontal='lg'>
-                        <ListGroup.Item>Listing ID: {listing.listing_id}</ListGroup.Item>
-
-                        <ListGroup.Item>Account ID: {listing.acc_id}</ListGroup.Item>
-
-                        <ListGroup.Item>Business: {listing.user.name}</ListGroup.Item>
-
-                        <ListGroup.Item>Address: {listing.user.address}</ListGroup.Item>
-
-                        <ListGroup.Item># Items: {listing.items.length}</ListGroup.Item>
-
-                        <ListGroup.Item>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                Items
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                {listing.items.map((item, i) =>
-                                    <Dropdown.Item>{i} || kg: {item.weight} || Exp: {item.expiry.toDateString()}</Dropdown.Item>
-                                )}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        </ListGroup.Item>
-
-                    </ListGroup>
-               )}
-            </Container>
-        )
-    }
-
-    // Response not received
     return (
-        <div>LOADING</div>
+        <div className="container">
+        <Accordion id="lv">
+            {allListings.map(listing => (
+                <Accordion.Item eventKey={listing.listing_id.toString() || "0"}>
+                    <Accordion.Header>
+                        {`#${listing.listing_id} - ${listing.user.name}`}
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <h6>Name: {listing.user.name}</h6>
+                        <h6>Phone: {listing.user.phone}</h6>
+                        <h6>Email: {listing.user.email}</h6>
+                        <h6>Container: {listing.container}</h6>
+                        <h6>Location: {listing.location}</h6>
+                        <p>{listing.notes}</p>
+                        <div className="container">
+                            <h5>Items</h5>
+                            <Table striped bordered hover responsive>
+                                <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Weight (kg)</th>
+                                    <th scope="col">Expiry</th>
+                                    <th scope="col">Tag</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {listing.items.map(item => (
+                                    <tr>
+                                        <td> {item.name} </td>
+                                        <td> {item.weight} </td>
+                                        <td> {new Date(item.expiry).toDateString()} </td>
+                                        <td> {item.tag_id} </td>
+                                    </tr>
+
+                                ))}
+                                </tbody>
+                            </Table>
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+        </div>
     )
+
 }
 
 export default ViewListings
